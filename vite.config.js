32f8c5da6d654731bed98ae6435262dd874380da
Mehdi <9340937+meduzen@users.noreply.github.com>
2022-07-23 13:24:26 +0200

    Migrate styles from `node-sass` to Dart SASS
    Also:
    * Update dependencies and improve Vite config documentation
    * Remove usage of local fonts
    * Remove unused SCSS code
    * Comment unused elements in CSS reset and base CSS

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,28 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-// env
+/**
+ * Parses .env file, using `dotenv`.
+ *
+ * We could use `loadEnv` (from 'vite'), but it exposes too much system values.
+ * Despite being used by vite under the hood, `dotenv` is among the project
+ * dev dependencies due to a version difference: Vite sticks to v14.3.2
+ * while the project uses v16.x.
+ *
+ * https://github.com/vitejs/vite/blob/main/packages/vite/package.json#L91
+ * https://github.com/motdotla/dotenv/blob/master/CHANGELOG.md
+ */
 const env = require('dotenv').config().parsed
-const isProd = env.NODE_ENV === 'production'
+// const isProd = env.NODE_ENV === 'production'
 
 let outDir = env.APP_BUILD_DIR || 'public'
 
 // Prevents to output app files outside of the project root.
 if (outDir.includes('../')) {
-  throw new Error('APP_BUILD_DIR (in the .env file) can’t point to an upper-level directory. Remove all `../`.')
+  throw new Error('APP_BUILD_DIR (in ./.env) directory must stays inside root level. To fix this error, remove all `../` from `APP_BUILD_DIR`.')
 }
 
-// helper to root project path
+// Shortcut to project root path
 const thePath = (path = '') => resolve(__dirname, path)
 
 export default defineConfig({
