39d6c3014b293a22d86dd1400748b8502e80df8c
Mehdi M <hi@mehdi.cc>
2022-11-22 21:07:29 +0100

    Test URLs validity

--- tests/links.spec.mjs
+++ tests/links.spec.mjs
@@ -9,6 +9,26 @@ test('All links have a non-empty `href` attribute', async ({ page }) => {
   await expect(badLinks).toBeNull()
 })
 
+test('All links are valid `href` attribute', async ({ page }) => {
+  const links = await page.$$('a[href]')
+
+  const invalidUrls = []
+  for (let index = 0; index < links.length; index++) {
+    const url = await links[index].getAttribute('href')
+
+    try { new URL(url) }
+    catch (error) { invalidUrls.push(url) }
+  }
+
+  // Show wrong URLs.
+  if (invalidUrls.length) {
+    const urlStr = invalidUrls.length > 1 ? 'URLs' : 'URL'
+    throw(`${invalidUrls.length} invalid ${urlStr}: ${invalidUrls.join(', ')}`)
+  }
+
+  await expect(null).toBeNull()
+})
+
 test('It links to the source code from the footer', async ({ page }) => {
   const repositoryLink = page.locator('footer [itemprop="codeRepository"]')
   await expect(repositoryLink).toHaveAttribute('href', /github.com/)
