398904e27b9d1122e38345c08e8824e3a904fa7e
Mehdi M <hi@mehdi.cc>
2023-04-07 15:51:26 +0200

    Update Playwright and tests folder structure

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -1,32 +1,46 @@
 import dotenv from 'dotenv';
 import { devices } from '@playwright/test'
+import { isInvalidUrl } from './tests/utils/url.mjs';
 
+/**
+ * Inject `.env` file entries into `process.env` if `.env` file exists.
+ * If a variable is transmitted through the command line and already
+ * exists in the `.env` file, the one from the command line wins.
+ */
 dotenv.config()
 
+const baseURL = process.env.PW_BASE_URL
+
+if (isInvalidUrl(baseURL)) {
+  throw new Error(`PW_BASE_URL is not a valid URL: ${baseURL}`)
+}
+
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
+export default {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : 1,
+
+  // tons of interesting options: https://playwright.dev/docs/api/class-testoptions
   use: {
-    baseURL: process.env.PW_BASE_URL,
+    baseURL,
     ignoreHTTPSErrors: true,
     trace: 'on-first-retry',
   },
+  preserveOutput: 'never',
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
+  reporter: process.env.CI ? 'github' : 'list',
 }
-
-export default config
