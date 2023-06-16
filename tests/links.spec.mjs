8bbf3a4988eef6228d5d1238dc966df257c0b74e
Mehdi M <hi@mehdi.cc>
2023-06-16 17:27:49 +0200

    Update links tests

--- tests/links.spec.mjs
+++ tests/links.spec.mjs
@@ -1,61 +1,68 @@
 import { test, expect } from '@playwright/test'
-import { isNotAnchorLink, isInvalidUrl, getAllHrefAttr } from './utils/url.mjs'
+import { isNotAnchorLink, isInvalidUrl, getAllHrefAttr, isAnchorLink } from './utils/url.mjs'
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/')
-})
+test.describe('links', () => {
+  /**
+   * @type {import('@playwright/test').Page}
+   */
+  let page
 
-test('All links have a non-empty `href` attribute', async ({ page }) => {
-  const badLinksCount = await page.locator('css=a:not([href]), a[href=""]').count()
-  expect(badLinksCount).toBe(0)
-})
+  const $links = () => page.locator('a')
+  const $anchorLinks = () => page.locator('a[href^="#"]') // `href=#…`
 
-test('All external links have a valid URL', async ({ page }) => {
-  const a = page.locator('a')
+  test.beforeAll(async ({ browser }) => page = await browser.newPage())
+  test.afterAll(async () => await page.close())
+  test.beforeEach(async () => await page.goto('/'))
 
-  const externalUrls = (await a.evaluateAll(getAllHrefAttr)).filter(isNotAnchorLink)
+  test('All links have a valid `href` attribute', async () => {
+    // @todo: improve this
+    const badNonAnchorLinks = (await $links().evaluateAll(getAllHrefAttr)).filter(isInvalidUrl).filter(isNotAnchorLink)
+    expect(badNonAnchorLinks).toHaveLength(0)
+  })
 
-  const invalidUrls = externalUrls.filter(isInvalidUrl)
+  test('All external links have a valid URL', async () => {
+    const externalUrls = (await $links().evaluateAll(getAllHrefAttr)).filter(isNotAnchorLink)
 
-  // Prepare error message with the list of invalid URLs.
+    const invalidUrls = externalUrls.filter(isInvalidUrl)
 
-  if (invalidUrls.length) {
-    const urlStr = invalidUrls.length == 1 ? 'URL' : 'URLs'
-    var errorStr = `${invalidUrls.length} invalid ${urlStr}: ${invalidUrls.join(', ')}`
-  }
+    // Prepare error message with the list of invalid URLs.
 
-  expect(externalUrls, errorStr).toHaveLength(externalUrls.length - invalidUrls.length)
-})
+    if (invalidUrls.length) {
+      const urlStr = invalidUrls.length == 1 ? 'URL' : 'URLs'
+      var errorStr = `${invalidUrls.length} invalid ${urlStr}: ${invalidUrls.join(', ')}`
+    }
 
-test('Each anchor link has 1 matching target', async ({ page }) => {
-  const a = page.locator('a[href^="#"]') // `href` starting by `#`.
+    expect(externalUrls, errorStr).toHaveLength(externalUrls.length - invalidUrls.length)
+  })
 
-  const hashes = await a.evaluateAll(getAllHrefAttr)
+  test('Each anchor link has 1 matching target', async () => {
+    const hashes = await $anchorLinks().evaluateAll(getAllHrefAttr)
 
-  // Check the targets of anchor links. Each hash must have exactly 1 target.
+    // Check the targets of anchor links. Each hash must have exactly 1 target.
 
-  const invalidHashs = []
+    const invalidHashs = []
 
-  await Promise.allSettled(hashes.map(hash =>
-    page.locator(hash)
-      .count()
-      .then(count => {
-        if (count !== 1) { throw new Error() }
-      })
-      .catch(() => invalidHashs.push(hash))
-  ))
+    await Promise.allSettled(hashes.map(hash =>
+      page.locator(hash)
+        .count()
+        .then(count => {
+          if (count !== 1) { throw new Error() }
+        })
+        .catch(() => invalidHashs.push(hash))
+    ))
 
-  // Prepare error message with the list of invalid hashes.
+    // Prepare error message with the list of invalid hashes.
 
-  if (invalidHashs.length) {
-    const hashStr = invalidHashs.length == 1 ? 'hash' : 'hashes'
-    var errorStr = `${invalidHashs.length} invalid ${hashStr}: ${invalidHashs.join(', ')}`
-  }
+    if (invalidHashs.length) {
+      const hashStr = invalidHashs.length == 1 ? 'hash' : 'hashes'
+      var errorStr = `${invalidHashs.length} invalid ${hashStr}: ${invalidHashs.join(', ')}`
+    }
 
-  expect(hashes, errorStr).toHaveLength(hashes.length - invalidHashs.length)
-})
+    expect(invalidHashs, errorStr).toHaveLength(0)
+  })
 
-test('It links to the source code from the footer', async ({ page }) => {
-  const repositoryLink = page.locator('footer [itemprop="codeRepository"]')
-  await expect(repositoryLink).toHaveAttribute('href', /github.com/)
+  test('It links to the source code from the footer', async () => {
+    const repositoryLink = page.locator('footer [itemprop="codeRepository"]')
+    await expect(repositoryLink).toHaveAttribute('href', /github.com/)
+  })
 })
